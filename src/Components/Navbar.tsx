f75f59361287bd365ced917c1cfe4d421e36a814
ujjwal <ujjwalppokharel@gmail.com>
2023-03-06 19:34:40 +0545

    first commit

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { GoThreeBars } from "react-icons/go";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex items-center w-full space-x-5 h-[80px] bg-[#ffffff]">
+        <GoThreeBars className="w-10 h-10 ml-8 text-[#1F9CEE]" />
+        <div className="flex items-center  ">
+          <img
+            src="smlogo.png"
+            alt="logo"
+            className="w-56 h-56 object-contain"
+          />
+        </div>
+      </div>
+      <div>
+        <img src="smbanner.png" alt="logos " />
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
