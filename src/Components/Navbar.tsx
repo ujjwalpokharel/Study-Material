d228e354da5ae8f952e398ef9749e5b754055b63
ujjwal <ujjwalppokharel@gmail.com>
2023-03-06 22:21:31 +0545

    nav layot created

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoThreeBars } from "react-icons/go";
 
 const Navbar = () => {
+  const [navpop, setNavpop] = useState(false);
   return (
     <>
-      <div className="flex items-center w-full space-x-5 h-[70px] bg-[#ffffff]">
-        <GoThreeBars className="w-10 h-10 ml-4 text-[#1F9CEE]" />
-        <div className="flex items-center  ">
+      <div className="relative ">
+        <div className="flex  items-center w-full space-x-5 h-[70px] bg-[#ffffff]">
+          <GoThreeBars
+            className="w-10 h-10 ml-4 text-[#1F9CEE]"
+            onClick={() => setNavpop(true)}
+          />
+
           <img
             src="smlogo.png"
             alt="logo"
             className="w-56 h-56 object-contain"
           />
         </div>
+        {navpop ? (
+          <div className="w-[200px] h-screen z-10 absolute top-0 bg-white">
+            <ul>
+              <li>home</li>
+              <li>about</li>
+              <li>page</li>
+            </ul>
+            <div
+              className="absolute right-0
+            top-0 bg-slate-500"
+            >
+              <button onClick={() => setNavpop(false)}>cancel</button>
+            </div>
+          </div>
+        ) : null}
       </div>
-      <div>
+
+      <div className="z-[1]">
         <img src="smbanner.png" alt="logos " />
       </div>
     </>
