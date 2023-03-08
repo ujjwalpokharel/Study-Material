aee0aa1317c4165d8a2ba33afcb4b1c30eee1f8f
ujjwal <ujjwalppokharel@gmail.com>
2023-03-08 16:04:03 +0545

    55

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import aboutustemplate from "../assets/AboutTemplate.png";
+import aboutustemplate from "../assets/AboutTemplatenew.png";
 import smyoutube from "../assets/smyoutube.jpg";
 
 const AboutUs = () => {
@@ -7,7 +7,7 @@ const AboutUs = () => {
     <div className="pt-[5rem] ">
       <img src={aboutustemplate} alt="abouttemplate" className="w-screen " />
       <section className="p-5">
-        <h1 className="font-bold text-4xl pb-5">Who we are:</h1>
+        <h1 className="font-bold text-2xl pb-5">About Study Material</h1>
         <p className="pb-3">
           We are a team of dedicated educators and professionals who are
           committed to providing high-quality study materials, resources, and
@@ -16,24 +16,24 @@ const AboutUs = () => {
           - it's a comprehensive platform where BBS students can access reliable
           and up-to-date information to enhance their learning experience.
         </p>
-        <a
-          href="https://www.youtube.com/@Studymaterialbypraju"
-          target={"_blank"}
-        >
-          <img src={smyoutube} alt="youtube image" />{" "}
-        </a>
-        <p className="pt-3 text-lg">
-          Our YouTube channel where we upload video lectures, tutorials, and
-          other informative content related to the BBS course.{" "}
-          <span className="font-bold text-[#1F9CEE]">
-            Click on the image to visit our youtube channel.
-          </span>
-        </p>
+        <div>
+          <a
+            href="https://www.youtube.com/@Studymaterialbypraju"
+            target={"_blank"}
+          >
+            <img src={smyoutube} alt="youtube image" />
+          </a>
+          <p className="pt-3 text-lg">
+            Our YouTube channel where we upload video lectures, tutorials, and
+            other informative content related to the BBS course.
+            <span className="font-bold text-[#1F9CEE]">
+              Click on the image to visit our youtube channel.
+            </span>
+          </p>
+        </div>
       </section>
-      <section className="p-5">
-        <h2 className="font-bold text-4xl pb-5">
-          How we are different from the rest:
-        </h2>
+      <section className="p-5 pt-0">
+        <h2 className="font-bold text-2xl pb-5">How we are different:</h2>
         <p className="text-lg">
           What sets us apart is our commitment to providing students with the
           most relevant and up-to-date study materials, notes, and question
@@ -42,9 +42,7 @@ const AboutUs = () => {
           learning, and that's why we put in the effort to keep our website
           current and comprehensive. In addition to study materials, we provide
           TU-related news and other essential information that can help students
-          stay informed and stay ahead. Moreover, we are not just a website. We
-          encourage feedback and suggestions from our users and are committed to
-          constantly improving our content and services.
+          stay informed and stay ahead.
         </p>
       </section>
     </div>
