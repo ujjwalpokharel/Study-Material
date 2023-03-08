ec66f2cbf6c31385d0fd926756118087517eb184
ujjwal <ujjwalppokharel@gmail.com>
2023-03-08 14:54:21 +0545

    25

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import aboutustemplate from "../assets/AboutTemplate.png";
-
+import smyoutube from "../assets/smyoutube.png";
 const AboutUs = () => {
   return (
-    <div className="pt-[5rem] text-lg  ">
-      <img src={aboutustemplate} alt="abouttemplate" className="" />
-      <div className="p-5">
-        <h1 className="font-bold">Who we are:</h1>
-        <p className="">
+    <div className="pt-[5rem] ">
+      <img src={aboutustemplate} alt="abouttemplate" className="w-screen " />
+      <section className="p-5">
+        <h1 className="font-bold text-4xl pb-5">Who we are:</h1>
+        <p className="pb-3">
           We are a team of dedicated educators and professionals who are
           committed to providing high-quality study materials, resources, and
           guidance to students pursuing the Bachelor of Business Studies (BBS)
-          course. Our team comprises experts in the field of business studies,
-          with years of experience in academia and industry. Our website is more
-          than just a collection of study materials - it's a comprehensive
-          platform where BBS students can access reliable and up-to-date
-          information to enhance their learning experience. We also run a
-          YouTube channel where we upload video lectures, tutorials, and other
-          informative content related to the BBS course.
+          course. Our website is more than just a collection of study materials
+          - it's a comprehensive platform where BBS students can access reliable
+          and up-to-date information to enhance their learning experience.
         </p>
-
-        <h2 className="font-bold">How we are different from the rest:</h2>
-
-        <p>
+        <a
+          href="https://www.youtube.com/@Studymaterialbypraju"
+          target={"_blank"}
+        >
+          <img src={smyoutube} alt="youtube image" />
+        </a>
+        <p className="pt-3 text-lg">
+          Our YouTube channel where we upload video lectures, tutorials, and
+          other informative content related to the BBS course.{" "}
+          <span className="font-bold text-[#1F9CEE]">
+            Click on the image to visit our youtube channel.
+          </span>
+        </p>
+      </section>
+      <section className="p-5">
+        <h2 className="font-bold text-4xl pb-5">
+          How we are different from the rest:
+        </h2>
+        <p className="text-lg">
           What sets us apart is our commitment to providing students with the
           most relevant and up-to-date study materials, notes, and question
           models for both old and new courses. We understand that students often
@@ -30,19 +41,11 @@ const AboutUs = () => {
           learning, and that's why we put in the effort to keep our website
           current and comprehensive. In addition to study materials, we provide
           TU-related news and other essential information that can help students
-          stay informed and stay ahead. We believe that education is a dynamic
-          process that involves continuous learning and keeping up with the
-          latest developments in the academic world. Moreover, we are not just a
-          website - we are a community of learners. We encourage feedback and
-          suggestions from our users and are committed to constantly improving
-          our content and services. Our YouTube channel is a testament to our
-          dedication to providing a diverse range of learning resources to
-          students. In short, what sets us apart is our team of dedicated
-          experts, our commitment to providing reliable and up-to-date study
-          materials, and our dedication to building a supportive community of
-          learners
+          stay informed and stay ahead. Moreover, we are not just a website. We
+          encourage feedback and suggestions from our users and are committed to
+          constantly improving our content and services.
         </p>
-      </div>
+      </section>
     </div>
   );
 };
