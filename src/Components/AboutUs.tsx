354641cf2f6d6b938646d0d6a9f1cc7f5050302a
ujjwal <ujjwalppokharel@gmail.com>
2023-03-08 13:58:21 +0545

    23

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -1,43 +1,48 @@
 import React from "react";
+import aboutustemplate from "../assets/AboutTemplate.png";
 
 const AboutUs = () => {
   return (
-    <div className="pt-[5rem] p-6 text-lg  ">
-      <h1 className="font-bold">Who we are:</h1>
-      <p className="">
-        We are a team of dedicated educators and professionals who are committed
-        to providing high-quality study materials, resources, and guidance to
-        students pursuing the Bachelor of Business Studies (BBS) course. Our
-        team comprises experts in the field of business studies, with years of
-        experience in academia and industry. Our website is more than just a
-        collection of study materials - it's a comprehensive platform where BBS
-        students can access reliable and up-to-date information to enhance their
-        learning experience. We also run a YouTube channel where we upload video
-        lectures, tutorials, and other informative content related to the BBS
-        course.
-      </p>
+    <div className="pt-[5rem] text-lg  ">
+      <img src={aboutustemplate} alt="abouttemplate" className="" />
+      <div className="p-5">
+        <h1 className="font-bold">Who we are:</h1>
+        <p className="">
+          We are a team of dedicated educators and professionals who are
+          committed to providing high-quality study materials, resources, and
+          guidance to students pursuing the Bachelor of Business Studies (BBS)
+          course. Our team comprises experts in the field of business studies,
+          with years of experience in academia and industry. Our website is more
+          than just a collection of study materials - it's a comprehensive
+          platform where BBS students can access reliable and up-to-date
+          information to enhance their learning experience. We also run a
+          YouTube channel where we upload video lectures, tutorials, and other
+          informative content related to the BBS course.
+        </p>
 
-      <h2 className="font-bold">How we are different from the rest:</h2>
+        <h2 className="font-bold">How we are different from the rest:</h2>
 
-      <p>
-        What sets us apart is our commitment to providing students with the most
-        relevant and up-to-date study materials, notes, and question models for
-        both old and new courses. We understand that students often struggle to
-        find reliable resources to supplement their classroom learning, and
-        that's why we put in the effort to keep our website current and
-        comprehensive. In addition to study materials, we provide TU-related
-        news and other essential information that can help students stay
-        informed and stay ahead. We believe that education is a dynamic process
-        that involves continuous learning and keeping up with the latest
-        developments in the academic world. Moreover, we are not just a website
-        - we are a community of learners. We encourage feedback and suggestions
-        from our users and are committed to constantly improving our content and
-        services. Our YouTube channel is a testament to our dedication to
-        providing a diverse range of learning resources to students. In short,
-        what sets us apart is our team of dedicated experts, our commitment to
-        providing reliable and up-to-date study materials, and our dedication to
-        building a supportive community of learners
-      </p>
+        <p>
+          What sets us apart is our commitment to providing students with the
+          most relevant and up-to-date study materials, notes, and question
+          models for both old and new courses. We understand that students often
+          struggle to find reliable resources to supplement their classroom
+          learning, and that's why we put in the effort to keep our website
+          current and comprehensive. In addition to study materials, we provide
+          TU-related news and other essential information that can help students
+          stay informed and stay ahead. We believe that education is a dynamic
+          process that involves continuous learning and keeping up with the
+          latest developments in the academic world. Moreover, we are not just a
+          website - we are a community of learners. We encourage feedback and
+          suggestions from our users and are committed to constantly improving
+          our content and services. Our YouTube channel is a testament to our
+          dedication to providing a diverse range of learning resources to
+          students. In short, what sets us apart is our team of dedicated
+          experts, our commitment to providing reliable and up-to-date study
+          materials, and our dedication to building a supportive community of
+          learners
+        </p>
+      </div>
     </div>
   );
 };
